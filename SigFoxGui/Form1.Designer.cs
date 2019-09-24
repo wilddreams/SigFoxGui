27750a10dbbfe411ac161e13369f26b7aad71eea
wilddreams <wilddreams@gmail.com>
2019-09-24 16:24:14 +0200

    the first commit of version 1.0 of SigFoxGui

---
+++ SigFoxGui/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace SigFoxGui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btSearch = new System.Windows.Forms.Button();
+            this.btScan = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbDevices = new System.Windows.Forms.ComboBox();
+            this.tbCommand = new System.Windows.Forms.TextBox();
+            this.btSendCommand = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Property = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btClearLog = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btSearch
+            // 
+            this.btSearch.Location = new System.Drawing.Point(245, 32);
+            this.btSearch.Name = "btSearch";
+            this.btSearch.Size = new System.Drawing.Size(73, 24);
+            this.btSearch.TabIndex = 0;
+            this.btSearch.Text = "Search";
+            this.btSearch.UseVisualStyleBackColor = true;
+            this.btSearch.Click += new System.EventHandler(this.btSearch_Click);
+            // 
+            // btScan
+            // 
+            this.btScan.Location = new System.Drawing.Point(324, 32);
+            this.btScan.Name = "btScan";
+            this.btScan.Size = new System.Drawing.Size(96, 23);
+            this.btScan.TabIndex = 1;
+            this.btScan.Text = "Scan Device";
+            this.btScan.UseVisualStyleBackColor = true;
+            this.btScan.Click += new System.EventHandler(this.btScan_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Serial Links:";
+            // 
+            // cbDevices
+            // 
+            this.cbDevices.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbDevices.FormattingEnabled = true;
+            this.cbDevices.Location = new System.Drawing.Point(82, 35);
+            this.cbDevices.Name = "cbDevices";
+            this.cbDevices.Size = new System.Drawing.Size(157, 21);
+            this.cbDevices.TabIndex = 3;
+            // 
+            // tbCommand
+            // 
+            this.tbCommand.Location = new System.Drawing.Point(15, 64);
+            this.tbCommand.Name = "tbCommand";
+            this.tbCommand.Size = new System.Drawing.Size(303, 20);
+            this.tbCommand.TabIndex = 4;
+            this.tbCommand.Text = "AT";
+            // 
+            // btSendCommand
+            // 
+            this.btSendCommand.Location = new System.Drawing.Point(324, 61);
+            this.btSendCommand.Name = "btSendCommand";
+            this.btSendCommand.Size = new System.Drawing.Size(96, 23);
+            this.btSendCommand.TabIndex = 5;
+            this.btSendCommand.Text = "Send Command";
+            this.btSendCommand.UseVisualStyleBackColor = true;
+            this.btSendCommand.Click += new System.EventHandler(this.btSendCommand_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(16, 102);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(403, 303);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listBox1_KeyDown);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Property,
+            this.Value});
+            this.dataGridView1.Location = new System.Drawing.Point(451, 32);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(464, 373);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Property
+            // 
+            this.Property.HeaderText = "Property";
+            this.Property.Name = "Property";
+            this.Property.ReadOnly = true;
+            this.Property.Width = 200;
+            // 
+            // Value
+            // 
+            this.Value.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Value.HeaderText = "Value";
+            this.Value.Name = "Value";
+            this.Value.ReadOnly = true;
+            // 
+            // btClearLog
+            // 
+            this.btClearLog.Location = new System.Drawing.Point(344, 418);
+            this.btClearLog.Name = "btClearLog";
+            this.btClearLog.Size = new System.Drawing.Size(75, 23);
+            this.btClearLog.TabIndex = 8;
+            this.btClearLog.Text = "Clear Log";
+            this.btClearLog.UseVisualStyleBackColor = true;
+            this.btClearLog.Click += new System.EventHandler(this.btClearLog_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(931, 453);
+            this.Controls.Add(this.btClearLog);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btSendCommand);
+            this.Controls.Add(this.tbCommand);
+            this.Controls.Add(this.cbDevices);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btScan);
+            this.Controls.Add(this.btSearch);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "SigFox Tester";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btSearch;
+        private System.Windows.Forms.Button btScan;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbDevices;
+        private System.Windows.Forms.TextBox tbCommand;
+        private System.Windows.Forms.Button btSendCommand;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Property;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+        private System.Windows.Forms.Button btClearLog;
+    }
+}
+
---
+++ SigFoxGui/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace SigFoxGui
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.btScan.Enabled = false;
+            this.btSendCommand.Enabled = false;
+
+        }
+
+        private void btSearch_Click(object sender, EventArgs e)
+        {
+            //clear the device list
+            this.cbDevices.Items.Clear();
+            this.cbDevices.Enabled = false;
+            //  Get the list of the serial port available
+            string[] ports = SerialPort.GetPortNames();
+            if (ports.Length > 0)
+            {
+                // Display each port name to the console.
+                foreach (string port in ports)
+                {
+                    this.cbDevices.Items.Add(port);
+                    this.cbDevices.SelectedIndex = 0;
+                }
+                this.cbDevices.Enabled = true;
+                this.btScan.Enabled = true;
+                this.btSendCommand.Enabled = true;
+            }
+            else
+            {
+                this.listBox1.Items.Add("No serial port detected");
+                this.listBox1.SelectedIndex = this.listBox1.Items.Count - 1; 
+                this.btScan.Enabled = false;
+                this.btSendCommand.Enabled = false;
+            }
+        }
+
+        private void btScan_Click(object sender, EventArgs e)
+        {
+            string PortName = null;
+            //Veryfy that any COM is selected.
+            try { PortName = this.cbDevices.SelectedItem.ToString(); }
+            catch
+            {
+                this.listBox1.Items.Add("It looks like no port is selected.");
+                this.listBox1.SelectedIndex = this.listBox1.Items.Count - 1;
+                return;
+            }
+
+            // Creating the serial port and setting the time out for 2s.
+            SerialPort port = new SerialPort(PortName, 9600, Parity.None, 8, StopBits.One);
+            port.ReadTimeout = 2000;
+
+            //try to open the serial port. If it fails the rutine stops and generates warning.
+            string ATCMessage = null;
+            try { 
+                port.Open();
+            } catch
+            {
+                this.listBox1.Items.Add("Unable to open Serial Port. Please search devices again.");
+                this.listBox1.SelectedIndex = this.listBox1.Items.Count - 1;
+                return;
+            }
+
+            //Sending the AT command to check for the correct answer.
+            //the code check if there is a propper answer from the device and if there is no time out.
+
+            this.listBox1.Items.Add("Sending AT command to the device.");
+            listBox1.SelectedIndex = listBox1.Items.Count - 1;
+            port.WriteLine("AT");
+            try
+            {
+                ATCMessage = port.ReadLine();
+                if (ATCMessage.CompareTo("OK\r")==0)
+                {
+                    this.listBox1.Items.Add("Device detected. Continuing.");
+                    this.listBox1.SelectedIndex = this.listBox1.Items.Count - 1;
+                } else
+                {
+                    this.listBox1.Items.Add("Strange reply from device: "+ATCMessage+" stopping");
+                    this.listBox1.SelectedIndex = this.listBox1.Items.Count - 1;
+                    port.Close();
+                    return;
+                }
+            } catch
+            {
+                this.listBox1.Items.Add("Serial time out. Stopping.");
+                this.listBox1.SelectedIndex = this.listBox1.Items.Count - 1;
+                port.Close();
+                return;
+            }
+            this.listBox1.Items.Add(ATCMessage);
+            this.listBox1.SelectedIndex = this.listBox1.Items.Count - 1;
+
+            //Get the information block 
+            // AT$I=uint 0-11
+            //
+            //0: Software Name &Version Example Response: AX - SFEU 1.0.6 - ETSI 
+            //1: Contact Detail Example Response:  info @lpwan.cz 
+            //2: Silicon revision lower byte Example Response: 8F 
+            //3: Silicon revision upper byte Example Response: 00 
+            //4: Major Firmware Version Example Response: 1 
+            //5: Minor Firmware Version Example Response: 0 
+            //7: Firmware Variant(Frequency Band etc. (EU/ US)) Example Response: ETSI 
+            //8: Firmware VCS Version Example Response: V1.0.2 - 36
+            //9: SIGFOX Library Version Example Response: DL0 - 1.4
+            //10: Device ID Example Response: 00012345
+            //11: PAC Example Response: 0123456789ABCDEF
+
+
+            Dictionary<string, string> dict1 = new Dictionary<string, string>();
+            dict1.Add("AT$I=0", "Software Name and Version");
+            dict1.Add("AT$I=1", "Contact Detail");
+            dict1.Add("AT$I=2", "Silicon revision lower byte");
+            dict1.Add("AT$I=3", "Silicon revision upper byte");
+            dict1.Add("AT$I=4", "Major Firmware Version");
+            dict1.Add("AT$I=5", "Minor Firmware Version");
+            dict1.Add("AT$I=7", "Firmware Variant");
+            dict1.Add("AT$I=8", "Firmware VCS Version");
+            dict1.Add("AT$I=9", "SIGFOX Library Version");
+            dict1.Add("AT$I=10", "Device ID");
+            dict1.Add("AT$I=11", "PAC");
+            dict1.Add("AT$V?", "Voltages");
+            dict1.Add("AT$T?", "Temperature");
+            dict1.Add("ATS300?", "Out Of Band Period");
+            dict1.Add("ATS302?", "Power Level");
+
+            this.dataGridView1.Rows.Clear();
+
+            foreach (KeyValuePair<string,string> ATCom in dict1)
+            {
+                string result = null;
+                try
+                {
+                    port.WriteLine(ATCom.Key);
+                    result = port.ReadLine();
+                } catch
+                {
+                    this.listBox1.Items.Add("Error during fetching data.");
+                    this.listBox1.SelectedIndex = this.listBox1.Items.Count - 1;
+                    port.Close();
+                    return;
+                }
+                this.dataGridView1.Rows.Add(ATCom.Value, result);
+            }
+            port.Close();
+        }
+
+        private void btSendCommand_Click(object sender, EventArgs e)
+        {
+            string PortName=null;
+            string result = null;
+            SerialPort port = null;
+            try {  PortName = this.cbDevices.SelectedItem.ToString(); }
+            catch
+            {
+                this.listBox1.Items.Add("It looks like no port is selected.");
+                this.listBox1.SelectedIndex = this.listBox1.Items.Count - 1;
+                return;
+            }
+
+            try
+            {
+                port = new SerialPort(PortName, 9600, Parity.None, 8, StopBits.One);
+                port.ReadTimeout = 20000;
+            } catch
+            {
+                this.listBox1.Items.Add("Unable to create serial port");
+                this.listBox1.SelectedIndex = this.listBox1.Items.Count - 1;
+                return;
+            }
+
+            try
+            {
+                port.Open();
+            } catch
+            {
+                this.listBox1.Items.Add("Unable to open serial port");
+                this.listBox1.SelectedIndex = this.listBox1.Items.Count - 1;
+                return;
+            }
+
+            try
+            {
+                port.WriteLine(tbCommand.Text);
+                result = port.ReadLine();
+            } catch
+            {
+                this.listBox1.Items.Add("Transmission failed");
+                this.listBox1.SelectedIndex = this.listBox1.Items.Count - 1;
+                port.Close();
+                return;
+            }
+            this.listBox1.Items.Add(result);
+            this.listBox1.SelectedIndex = this.listBox1.Items.Count - 1;
+            port.Close();
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btClearLog_Click(object sender, EventArgs e)
+        {
+            this.listBox1.Items.Clear();
+            //this.listBox1.SelectedIndex = 0;
+        }
+
+        private void listBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control == true && e.KeyCode == Keys.C)
+            {
+                string s = listBox1.SelectedItem.ToString();
+                Clipboard.SetData(DataFormats.StringFormat, s);
+            }
+        }
+    }
+}
